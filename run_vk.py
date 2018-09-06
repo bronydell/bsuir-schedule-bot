66655e0576065c41ed9988ec365d6fb76d512491
Rostislav Nikishin <littleponyapps@gmail.com>
2018-09-06 10:08:45 +0300

    Added try catch just to be sure that it won't fail

--- run_vk.py
+++ run_vk.py
@@ -35,7 +35,11 @@ def get_client(session):
 
 def start_polling(client, vk_poll):
     for event in vk_poll.listen():
-        on_vk_event(client, event)
+        try:
+            on_vk_event(client, event)
+        except Exception as ex:
+            print(ex)
+            pass
 
 
 def main():
